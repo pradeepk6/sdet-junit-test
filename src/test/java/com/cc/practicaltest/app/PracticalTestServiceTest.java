0e001563810d04e3389395088513c9cee0d4bb61
Jim Trotman <james.trotman@costcutter.com>
2019-05-02 15:47:11 +0100

    Added placeholder test

---
+++ src/test/java/com/cc/practicaltest/app/PracticalTestServiceTest.java
@@ -0,0 +1,15 @@
+package com.cc.practicaltest.app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PracticalTestServiceTest {
+    @Test
+    public void willAlwaysPass() {
+        assertTrue(true);
+    }
+}
